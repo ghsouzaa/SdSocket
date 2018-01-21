30ca8b8e8c94ffa1973f2fa89de3ec8f6d6cbef0
Gabriel Henrique <ghsouza@outlook.com>
2018-01-21 00:29:41 -0300

    Commit incial

---
+++ ClientA/ClientA.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class ClientA
+{
+    public static void Main(String[] args)
+    {
+        #region Variaveis
+
+        byte[] bytes = new byte[1024];
+        IPAddress ipServidor = IPAddress.Parse("192.168.56.1");
+        int portaServidor = 11000;
+        string tipoClient = "A";
+
+        #endregion
+
+        try
+        {
+            // Cria EndPoint que será usado pelo socket.
+            IPEndPoint endPointServidor = new IPEndPoint(ipServidor, portaServidor);
+
+            // Cria socket. 
+            Socket sender = new Socket(ipServidor.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Conecta o socket com o servidor.
+            sender.Connect(endPointServidor);
+
+            Console.WriteLine("Conectado ao servidor {0}", sender.RemoteEndPoint.ToString());
+
+            // Envia o tipo do cliente.
+            sender.Send(Encoding.ASCII.GetBytes(tipoClient));
+
+            // Laço para ficar requisitando o servidor.
+            while (true)
+            {
+                Console.Write("Digite um número ou aperte Enter para sair: ");
+                var op = Console.ReadLine();
+
+                // Condição para saber se deseja desconectar.
+                if (op != "")
+                {
+                    // Envia mensagem para o servidor.
+                    byte[] msg = Encoding.ASCII.GetBytes(op);
+                    int bytesSent = sender.Send(msg);
+
+                    // Recebe o resultado e mostra na tela.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Resultado = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                }
+                else
+                {
+                    // Envia mensagem para o servidor avisando que quer se desconectar.
+                    byte[] msg = Encoding.ASCII.GetBytes(op);
+                    int bytesSent = sender.Send(msg);
+                    break;
+                }
+            }
+
+            // Desconecta do servidor.
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Socket/Server.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Server
+{
+    public static void StartListening()
+    {
+        #region Variaveis
+
+        string mensagem = null;
+        byte[] bytesMensagem = new Byte[1024];
+
+        string tipoCliente = null;
+        byte[] bytesTipoCliente = new Byte[1024];
+
+        IPAddress ipLocal = LocalIPAddress();
+        int portaServidor = 11000;
+
+        #endregion
+
+        // Cria EndPoint local que será usado pelo socket.
+        IPEndPoint endPoint = new IPEndPoint(ipLocal, portaServidor);
+
+        // Cria Socket.
+        Socket listener = new Socket(ipLocal.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            // Vincula o Socket ao Endpoint do servidor.
+            listener.Bind(endPoint);
+            // Limite da fila de clientes conectados.
+            listener.Listen(10);
+
+            // Laço para ficar aguardando conexões dos clientes.
+            while (true)
+            {
+                Console.WriteLine("Esperando conexão de um cliente...");
+                // Servidor fica parado nesse ponto esperando a conexão de um novo cliente.  
+                Socket handler = listener.Accept();
+                
+                // Reseta o tipo do cliente pra cada nova conexão receber um novo tipo.
+                bytesTipoCliente = new byte[1024];
+
+                // Recebe os bytes do tipo do client, transforma em string e salva na variavel tipoCliente.
+                int bytesRecTipo = handler.Receive(bytesTipoCliente);
+                tipoCliente = Encoding.ASCII.GetString(bytesTipoCliente, 0, bytesRecTipo);
+                Console.WriteLine("Cliente tipo " + tipoCliente.ToUpper() + " conectado.");
+
+                // Loop que fica aguardando as requisições do cliente conectado até que decida desconectar.
+                while (true)
+                {
+                    // Reseta a mensagem para cada nova requisição receber uma nova mensagem.
+                    bytesMensagem = new byte[1024];
+
+                    // Recebe os bytes da mensagem, transforma em string e salva na variavel mensagem.
+                    int bytesRecMensagem = handler.Receive(bytesMensagem);
+                    mensagem = Encoding.ASCII.GetString(bytesMensagem, 0, bytesRecMensagem);
+
+                    // Condição pra saber se o cliente deseja desconectar.
+                    if (mensagem == "")
+                    {
+                        // Desconecta do cliente e para esse laço pra voltar ao laço de aguardando conexão.
+                        Console.WriteLine("Cliente desconectou.");
+                        Console.WriteLine("");
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        break;
+                    }
+                    else
+                    {
+                        byte[] byteResultado = null;
+                        Console.WriteLine("Mensagem recebida: {0}", mensagem);
+
+                        if(tipoCliente == "A")
+                        {
+                            // Converte a mensagem de String para Inteiro, aplica a operação e guarda o resultado na variavel de retorno, em bytes.
+                            int convert;
+                            int.TryParse(mensagem, out convert);
+                            int resultado = convert * convert;
+                            byteResultado = Encoding.ASCII.GetBytes(resultado.ToString());
+                        }
+                        else
+                        {
+                            // Converte a mensagem de String para Double, aplica a operação e guarda o resultado na variavel de retorno, em bytes.
+                            double convert;
+                            double.TryParse(mensagem, out convert);
+                            double resultado = Math.Sqrt(convert);
+                            byteResultado = Encoding.ASCII.GetBytes(resultado.ToString());
+                        }
+
+                        // Envia o resultado da operação para o cliente.
+                        handler.Send(byteResultado);
+                    }
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nOcorreu um erro...");
+        Console.Read();
+    }
+
+    /// <summary>
+    /// Retorna a classe IPAdress que contém o IP da máquina que está executando o método
+    /// </summary>
+    /// <returns></returns>
+    private static IPAddress LocalIPAddress()
+    {
+        if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            return null;
+
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+        return host.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+    }
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
